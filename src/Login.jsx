d0c3c226cc53944509a8d23f32790382d843da6b
xu yanfeng <xuyanfeng@Olivers-MacBook-Pro.local>
2023-03-08 11:19:16 -0500

    firstdraft

---
+++ src/Login.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+
+export const Login = (props) => {
+    const [email, setEmail] = useState('');
+    const [pass, setPass] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(email);
+    }
+
+    return (
+        <>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor='email'>Email</label>
+                <input value={email} onChange={(e) => setEmail(e.target.value)} type='email' placeholder='xxxxxx@gmail.com' id='email' name='email' />
+                <label htmlFor='password'>password</label>
+                <input value={pass} onChange={(e) => setPass(e.target.value)} type='password' placeholder='******' id='password' name='password' />
+                <button>Log In</button>
+            </form>
+            <button onClick={props.onFormSwitch}>Don't have an account? Register here</button>
+        </>
+    )
+}
